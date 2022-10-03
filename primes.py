80590d507b8f6d50608278402120dab349c78608
Burhan Tekcan <burhan.tekcan@kcl.ac.uk>
2022-10-03 23:15:29 +0100

    Raise exception if not positive

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def Prime(n):
+    for i in range(2,n//2+1):
+        if(n%i==0):
+            return(0)
+    return(1)
+
 def primes(number_of_primes):
     list = []
+    if number_of_primes <= 0:
+        raise ValueError('input is not positive')
+    i = 2
+    while(1):
+        if(Prime(i)):
+            list.append(i)
+            if(len(list)==number_of_primes):
+                break
+        i+=1
     return list
